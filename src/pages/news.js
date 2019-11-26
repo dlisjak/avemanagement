cbc22168d437cb51e225712acc7d7a7357ce5a16
Domen Lisjak <domen.lisjak@gmail.com>
2019-11-26 16:37:51 +0100

    grid css

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,29 +1,39 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
 
 const NewsPage = ({ data }) => {
+  const [isLoaded, setLoaded] = useState(false)
   useEffect(() => {
     // componentDidMount
-    const fetchData = async () => {}
-    fetchData()
+    setTimeout(() => {
+      setLoaded(true)
+    }, 1000)
   }, [])
 
   console.log(data)
 
+  const news = data.allWordpressWpNews.edges.map(
+    ({ node: { title, slug, acf } }, index) => (
+      <div
+        className="grid-item"
+        style={{
+          maxHeight: acf.news_post_image.height,
+          height: "100%",
+        }}
+      >
+        <Link to={`/${slug}`} key={index}>
+          <img src={acf.news_post_image.url} />
+        </Link>
+      </div>
+    )
+  )
+
   return (
     <Layout>
       <h2>News</h2>
-      <div className="grid">
-        {data.allWordpressWpNews.edges.map(
-          ({ node: { title, slug, acf } }, index) => (
-            <div className="grid-item">
-              <Link to={`/${slug}`} key={index}>
-                <img src={acf.news_post_image.url} />
-              </Link>
-            </div>
-          )
-        )}
+      <div className="grid" style={{ display: !isLoaded ? "none" : "block" }}>
+        {news}
       </div>
     </Layout>
   )
