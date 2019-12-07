21e3ac95bd487845a5e41403d2f8a56ab34d1a08
Domen Lisjak <domen.lisjak@gmail.com>
2019-12-07 13:26:02 +0100

    new fixed

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,13 +7,12 @@ const contact = () => {
       <div className="flex flex-column align-center">
         <h1>Ave Management</h1>
         <span>70 SHENTON WAY, #13-06 EON SHENTON SINGAPORE 079118</span>
+        <span>T +65 68874629</span>
+        <span>T +65 68874629</span>
 
-        <div className="flex flex-column align-center">
-          <span>T +65 68874629</span>
-          <span>T +65 68874629</span>
+        <a href="mailto:info@avemanagement.com">info@avemanagement.com</a>
 
-          <a href="mailto:info@avemanagement.com">info@avemanagement.com</a>
-        </div>
+        <a href="https://goo.gl/maps/JoLuAknYkKp">Google maps</a>
       </div>
     </Layout>
   )
