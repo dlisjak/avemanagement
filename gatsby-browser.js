1a2b29529cef172583708133844a1eab97457ac1
Domen Lisjak <domen.lisjak@gmail.com>
2020-01-26 01:55:53 +0100

    wtf

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,7 @@ import GlobalContextProvider from "./src/context/GlobalContextProvider"
 export const wrapRootElement = ({ element }) => {
   return <GlobalContextProvider>{element}</GlobalContextProvider>
 }
+
+export const onPreRouteUpdate = ({ location }) => {
+  console.log("Gatsby started to change location to", location.pathname)
+}
