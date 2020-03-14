5e626805bf46b3419d6ec25266193c125fbabe56
Domen Lisjak <domen.lisjak@gmail.com>
2020-03-14 18:19:44 +0100

    upload

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -20,33 +20,33 @@ const Loader = () => {
         left: 0,
         right: 0,
         bottom: 0,
-        width: isMobile ? "100%" : "80%",
+        width: "100%",
         height: "100vh",
         background: `white`,
         zIndex: 9999999999,
         alignItems: "center",
         justifyContent: "center",
-        paddingBottom: 100,
-        maxWidth: 1440,
-        margin: "auto",
       }}
     >
-      <img
-        key={1}
-        className="loading-logo"
-        src={Logo}
-        alt="AVE LOGO"
-        style={{
-          margin: "5px 5px 0px 0px",
-          cursor: "pointer",
-          width: "100%",
-          height: "auto",
-          maxWidth: 250,
-          position: isMobile ? "relative" : "absolute",
-          top: !isMobile && 0,
-          left: !isMobile && 0,
-        }}
-      />
+      <div
+        className="loadingContainer"
+        style={{ width: "80%", margin: "auto", height: "100%" }}
+      >
+        <img
+          key={1}
+          className="loading-logo"
+          src={Logo}
+          alt="AVE LOGO"
+          style={{
+            margin: "5px 5px 0px 0px",
+            cursor: "pointer",
+            width: "100%",
+            height: "auto",
+            maxWidth: 250,
+            position: "relative",
+          }}
+        />
+      </div>
     </div>
   )
 }
