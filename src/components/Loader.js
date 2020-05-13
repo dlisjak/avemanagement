67dd3c8149548d96604e727a5129b170180d82f6
Domen Lisjak <d.lisjak@emakina.at>
2020-05-13 22:38:12 +0200

    min css

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,12 +3,18 @@ import React, { useEffect } from "react"
 import Logo from "../images/logo.svg"
 
 const Loader = () => {
-  const bodyEl = document.querySelector("body")
-  bodyEl.classList.add("overlay")
+  useEffect(() => {
+    const displayLoader = () => {
+      const bodyEl = document.querySelector("body")
+      bodyEl.classList.add("overlay")
 
-  setTimeout(() => {
-    bodyEl.classList.remove("overlay")
-  }, 1500)
+      setTimeout(() => {
+        bodyEl.classList.remove("overlay")
+      }, 1500)
+    }
+
+    displayLoader()
+  })
 
   return (
     <div className="loaderMain flex flex-column" key={0}>
