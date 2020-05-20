42ad767dd527a2c31f99089a2503846ec2aedfaa
Domen Lisjak <d.lisjak@emakina.at>
2020-05-21 00:02:29 +0200

    bug

--- src/components/ModelPreviewVideos.js
+++ src/components/ModelPreviewVideos.js
@@ -1,33 +1,26 @@
 import React from "react"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 
-const ModelPreviewVideos = ({ thumbnail, onClick }) => {
-  let isMobile
-  if (typeof window !== "undefined") {
-    isMobile = window.innerWidth < 480
-  }
-
-  return (
-    <AnchorLink
-      href="#slideshow"
-      offset={27}
-      className="category-card grid-item model-preview"
-      onClick={onClick}
+const ModelPreviewVideos = ({ thumbnail, onClick }) => (
+  <AnchorLink
+    href="#slideshow"
+    offset={27}
+    className="category-card grid-item model-preview"
+    onClick={onClick}
+  >
+    <div
+      className="flex relative justify-center align-center model-previews--vid"
+      style={{
+        backgroundImage: `url(${thumbnail})`,
+      }}
     >
-      <div
-        className="flex relative justify-center align-center model-previews--vid"
-        style={{
-          backgroundImage: `url(${thumbnail})`,
-        }}
-      >
-        <div className="flex absolute model-previews--vid-inside align-center justify-center">
-          <div className="absolute model-previews--vid-inside-two" />
-          <div className="absolute model-previews--vid-inside-two -two" />
-          <div className="absolute model-previews--vid-inside -three" />
-        </div>
+      <div className="flex absolute model-previews--vid-inside align-center justify-center">
+        <div className="absolute model-previews--vid-inside-two" />
+        <div className="absolute model-previews--vid-inside-two -two" />
+        <div className="absolute model-previews--vid-inside -three" />
       </div>
-    </AnchorLink>
-  )
-}
+    </div>
+  </AnchorLink>
+)
 
 export default ModelPreviewVideos
--- src/components/NewsPreviewVideos.js
+++ src/components/NewsPreviewVideos.js
@@ -1,20 +1,18 @@
 import React from "react"
 
-const NewsPreviewVideos = ({ thumbnail }) => {
-  return (
-    <div
-      className="relative flex justify-center align-center news-preview-videos"
-      style={{
-        backgroundImage: `url(${thumbnail})`,
-      }}
-    >
-      <div className="absolute flex justify-center align-center news-preview-videos-inside">
-        <div className="absolute news-preview-videos-inside--two" />
-        <div className="absolute news-preview-videos-inside--three" />
-        <div className="absolute news-preview-videos-inside--four" />
-      </div>
+const NewsPreviewVideos = ({ thumbnail }) => (
+  <div
+    className="relative flex justify-center align-center news-preview-videos"
+    style={{
+      backgroundImage: `url(${thumbnail})`,
+    }}
+  >
+    <div className="absolute flex justify-center align-center news-preview-videos-inside">
+      <div className="absolute news-preview-videos-inside--two" />
+      <div className="absolute news-preview-videos-inside--three" />
+      <div className="absolute news-preview-videos-inside--four" />
     </div>
-  )
-}
+  </div>
+)
 
 export default NewsPreviewVideos
